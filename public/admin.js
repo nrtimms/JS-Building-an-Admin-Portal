eeb2a22092de70fb278798db0701f5100930c898
nrtimms <nataliertimms18@gmail.com>
2023-05-01 11:43:13 -0700

    created admin portal

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,34 @@
+//Fetching list of books
+async function main() {
+    let response = await fetch('http://localhost:3001/listBooks')
+    let books = await response.json()
+    books.forEach(renderBook)
+}
 
-// Your Code Here
+function renderBook(book) {
+    let root = document.querySelector('#root')
+    //create new elements
+    let li = document.createElement('li')
+    li.textContent = book.title
+    let quantityInput = document.createElement('input')
+    quantityInput.value = book.quantity
+    let saveButton = document.createElement('button')
+    saveButton.textContent = 'Save'
+
+    //update quantitiy of book when save button is clicked
+    saveButton.addEventListener('click', () => {
+        fetch('http://localhost:3001/updateBook', {
+            method: 'PATCH',
+            headers: {'Content-Type': 'application/json'},
+            body: JSON.stringify({
+                id: book.id,
+                quantity: quantityInput.value
+            })
+        })
+    })
+    //add new elements to the div with id=root
+    li.append(quantityInput, saveButton)
+    root.append(li)
+}
+
+main();
